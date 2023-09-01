cac271fb1f8276a9f784aa5311e028cca6e914a4
Rylie Castell <rylie.castell@gmail.com>
2023-09-01 10:43:28 +0200

    add students

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        Student angie = new Student("Angela", "Merkel", 161);
+        Student daddy = new Student("Gregor", "Gysi", 1312);
+        Student lilOldie = new Student("Philipp", "Amthor", 420);
+
+        List<Student> students = new ArrayList<>();
+
+        students.add(angie);
+        students.add(daddy);
+        students.add(lilOldie);
     }
+
+
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    String firstName;
+    String lastName;
+    int matrikelNumber;
+
+    public Student(String firstName, String lastName, int matrikelNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.matrikelNumber = matrikelNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getMatrikelNumber() {
+        return matrikelNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setMatrikelNumber(int matrikelNumber) {
+        this.matrikelNumber = matrikelNumber;
+    }
+
+
 }
